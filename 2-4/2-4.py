f4d92e9385a3bcfd31c68d6d259390a0540cec54
Oleksiy Lavrynenko <oleksiy.lavrynenko@outlook.com>
2017-07-08 00:12:41 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	2-2/2.json
    #	2-2/2.yaml

---
+++ 2-4/2-4.py
@@ -0,0 +1,24 @@
+import glob
+import os.path
+from pprint import pprint
+
+def get_a_list():
+    migrations = 'Migrations'
+    files = glob.glob(os.path.join(migrations, "*.sql"))
+    return files
+
+def main():
+    all_names_files = get_a_list()
+    while True:
+        all_results = list()
+        questions = input("Какой файл ищем?\n >>> ")
+        for file in all_names_files:
+            with open(file, 'r') as f:
+                line = f.read()
+                if questions in line:
+                    all_results.append(file)
+        all_names_files = all_results
+        pprint(all_results)
+        pprint("Всего найдено: {} файлов в all_results".format(len(all_results)))
+
+main()
